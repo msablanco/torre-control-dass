eaa7ce68005bfe5adb676b08fc8a215a352ab247
msablanco <mblanco@grupodass.com.ar>
2026-02-10 15:19:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if "GEMINI_API_KEY" in st.secrets:
     try:
         genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
         # Con la librería actualizada, este nombre ya no dará 404
-       model = genai.GenerativeModel('models/gemini-1.5-flash')
+        model = genai.GenerativeModel('gemini-1.5-flash')
     except Exception as e:
         st.error(f"Error al configurar Gemini: {e}")
 
@@ -343,6 +343,7 @@ if data:
 
 else:
     st.error("No se pudieron cargar los datos. Verifique la carpeta de Drive.")
+
 
 
 
